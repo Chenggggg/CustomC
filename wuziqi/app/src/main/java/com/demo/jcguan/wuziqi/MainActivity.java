ba4f30db51069b7035f4227ee0d89ab99426691c
Chenggggg <J_C_GUAN@163.com>
2016-08-04 07:14:42 +0800

    简易自定义控件——五子棋

--- wuziqi/app/src/main/java/com/demo/jcguan/wuziqi/MainActivity.java
+++ wuziqi/app/src/main/java/com/demo/jcguan/wuziqi/MainActivity.java
@@ -2,12 +2,26 @@ package com.demo.jcguan.wuziqi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WuziqiPanel mWuziqiPanel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mWuziqiPanel= (WuziqiPanel) findViewById(R.id.wzq);
+        Button bt = (Button) findViewById(R.id.bt);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mWuziqiPanel.reStart();
+            }
+        });
     }
 }
