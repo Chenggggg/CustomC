1590925ab645d865d249ed92718d07283ee9dfb3
Chenggggg <J_C_GUAN@163.com>
2016-08-04 18:20:11 +0800

    自定义水平进度条

--- SProgressbar/app/src/main/java/com/demo/jcguan/sprogressbar/RoundProgressbar.java
+++ SProgressbar/app/src/main/java/com/demo/jcguan/sprogressbar/RoundProgressbar.java
@@ -1,7 +1,25 @@
 package com.demo.jcguan.sprogressbar;
 
+import android.content.Context;
+import android.util.AttributeSet;
+
 /**
  * Created by jcguan on 2016/8/4.
  */
-public class RoundProgressbar {
+public class RoundProgressbar extends HorizantalProgressbar {
+
+    public RoundProgressbar(Context context, AttributeSet attrs) {
+        this(context, attrs,0);
+    }
+
+    public RoundProgressbar(Context context) {
+        this(context,null);
+    }
+
+    public RoundProgressbar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+
+    }
+
 }
